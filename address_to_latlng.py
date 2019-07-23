d23cb0061b536c82a457b39aead2c8e24ac83af3
root <root@dpovolotskiy.srt.mirantis.net>
2019-07-23 16:39:54 +0400

    Init commit

---
+++ address_to_latlng.py
@@ -0,0 +1,24 @@
+import geocoder
+import json
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="Path to input file (full or relative)", default="input.txt")
+parser.add_argument("-o", "--output", help="Path to output file (full or relative). Must be JSON file.",
+                    default="output.json")
+args = parser.parse_args()
+
+geo_list = []
+with open(str(args.input)) as geo_data:
+    for line in geo_data:
+        geo_list.append(line.rstrip())
+
+result = {}
+for address in geo_list:
+    g = geocoder.yandex(address)
+    result[address] = g.latlng
+with open(str(args.output), "w") as output:
+    output.write(json.dumps(result, ensure_ascii=False, sort_keys=True, indent=4))
+
+print("Works done!")
